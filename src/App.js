297455655d822369e60cb626bdf6e853d01d13d0
Shanthi S <130643002+Shanthi2814@users.noreply.github.com>
2023-06-16 10:14:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,16 +9,16 @@ const App=(props)=>{
     <hr/>
         <ul>
         <li><span class="fa-li"><i class="fas fa-check"></i></span>✔ Single User</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>✔ 5GB Storage</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>✔ Unlimited Public Projects</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>✔ Community Access</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>✖ Unlimited
                 Private Projects</li>
-              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>✖ Dedicated
                 Phone Support</li>
-              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>✖ Free Subdomain
               </li>
-              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>✖ Monthly Status
                 Reports</li>
         </ul>
         <button class="btn btn-primary">BUTTON</button>
@@ -29,14 +29,14 @@ const App=(props)=>{
         <h6 class="card-price text-center">$9<span class="period">/month</span></h6>
 <hr/>
         <ul>
-        <li><span class="fa-li"><i class="fas fa-check"></i></span>5 User</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>50GB Storage</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Free Subdomain</li>
-              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
+        <li><span class="fa-li"><i class="fas fa-check"></i></span>✔ 5 User</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>✔ 50GB Storage</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>✔ Unlimited Public Projects</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>✔ Community Access</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>✔ Unlimited Private Projects</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>✔ Dedicated Phone Support</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>✔ Free Subdomain</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>✖ Monthly Status
                 Reports</li>
         </ul>
         <button class="btn btn-primary">BUTTON</button>
@@ -49,14 +49,14 @@ const App=(props)=>{
         <ul>
         <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Unlimited Users</strong>
               </li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>150GB Storage</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Unlimited</strong> Free
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>✔ 150GB Storage</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>✔ Unlimited Public Projects</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>✔ Community Access</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>✔ Unlimited Private Projects</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>✔ Dedicated Phone Support</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>✔ Unlimited</strong> Free
                 Subdomains</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Monthly Status Reports</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>✔ Monthly Status Reports</li>
         </ul>
         <button class="btn btn-primary">BUTTON</button>
         {/* <a href="#" class="btn btn-primary">BUTTON</a> */}
